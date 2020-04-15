a0a0d9b8c4cc3824a8331a3991614dfeba024e48
old-mans <jdx982909501@163.com>
2020-04-15 10:34:39 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">express --version # 4.15.5</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>  |
       <router-link to="/admin">admin</router-link>
     </div>
