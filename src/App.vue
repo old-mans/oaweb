0ee0ccac2f86457c7d7cc7e2a3302d18275fcbe9
old-mans <jdx982909501@163.com>
2020-04-15 10:26:26 +0800

    web

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">express --version # 4.15.5</router-link> |
+      <router-link to="/about">About</router-link>  |
+      <router-link to="/admin">admin</router-link>
     </div>
     <router-view/>
   </div>
